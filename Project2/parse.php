61ab22c5ab6a5dff110721150d7a570fa78e0655
adisusilayasa <adisusilayasa@users.noreply.github.com>
2020-12-20 18:41:36 +0800

    Update parse.php

--- Project2/parse.php
+++ Project2/parse.php
@@ -293,7 +293,7 @@ if (isset($w)) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Noogle</title>
+    <title>Boogle</title>
     <link rel="stylesheet" href="css/bootstrap.css">
 </head>
 
